e41f94e0262877d10c46b559bfab84ca044e61f8
CanCelik24 <ogulcan.celik24@gmail.com>
2023-03-18 03:16:59 +0300

    hata düzeltme

--- js/main.js
+++ js/main.js
@@ -1,14 +1,21 @@
+
+//Kaybolan hoş geldiniz için fonksiyon
+
 $(document).ready(
     function () {
       setTimeout(function () { $('#welcome').hide('slow'); }, 5000);
     }
   );
 
+ 
+//sayfa yüklenmesi için oluşturulan fonsiyon 
+
 $(window).load(function(){
   $('.loader').hide(); 
   $('.myPage').show(); 
   });
 
+//Hakkımda kısmı için oluşturulan fonksiyon
 
   let i = 0;
   let txt = "    Merhaba, ben Oğulcan. Bana kısaca Can diye hitap edebilirsiniz. Araştıma yapmak ve yeni şeyler öğrenmeyi seviyorum. Hobi olarak başladığım bu yolculuğu mesleğim haline getirmeye çalışıyorum. HTML5, CSS3, Bootsrap, JavaScript, Ant Desing, Chakra UI, ReactJS gibi dil, kütüphaneler, API'ler ve araçlarla geliştirmeler yapıyorum. Şuan React ve Redux konularında kendimi geliştiriyorum. Frontend konusunda kendimi geliştirdikten sonra backend konularında kendimi geliştirmeye başlayacağım. Sizin de bu konular hakkında bana önerileriniz veya iş teklifleriniz varsa iletişime geçebilir, GitHub hesabımdan projelerimi inceleyebilirsiniz. Vakit ayırdığınız için teşekkür ederim. 😊";
